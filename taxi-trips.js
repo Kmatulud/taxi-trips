298877431f0db0ee777f2c0b75b2ae6aa0195cea
kmatulud <princessmakgotso@gmail.com>
2021-11-22 15:22:44 +0200

    initial

--- taxi-trips.js
+++ taxi-trips.js
@@ -1,3 +1,40 @@
-module.exports = function() {
-	
+module.exports = async function TaxiTrips() {
+    async function totalTripCount(){
+
+    }
+    async function findAllRegions(){
+
+    }
+    async function findTaxisForRegion(){
+
+    }
+    async function findTripsByRegNumber(){
+
+    }
+    async function findTripsByRegion(){
+
+    }
+    async function findIncomeByRegNumber(){
+
+    }
+    async function findTotalIncomePerTaxi(){
+
+    }
+    async function findTotalIncome(){
+
+    }
+    async function findTotalIncomeByRegion(){
+        
+    }
+    return {
+        totalTripCount,
+        findAllRegions,
+        findTripsByRegNumber,
+        findTripsByRegion,
+        findIncomeByRegNumber,
+        findTotalIncomePerTaxi,
+        findTotalIncome,
+        findTotalIncomeByRegion,
+        findTaxisForRegion
+    }
 }
